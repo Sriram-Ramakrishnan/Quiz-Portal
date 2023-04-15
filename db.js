7a27d7dac997dae33fec3b4265e4cf6767cfc6fc
Sriram Ramakrishnan <srirampro107@gmail.com>
2023-04-15 11:15:12 +0530

    15/04: Postgres connection setup

---
+++ db.js
@@ -0,0 +1,13 @@
+import config from './config';
+
+const Pool = require('pg').Pool;
+
+const pool = new Pool({
+    user: "postgres",
+    host: "localhost",
+    database: "questions",
+    password: config.pg_password, // Add your own postgres password here 
+    port: 5432,
+});
+
+module.exports = pool;
