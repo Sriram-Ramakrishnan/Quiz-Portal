c5a251258e3ec6103f28a1bfc31ca8f160c567cf
Sriram Ramakrishnan <srirampro107@gmail.com>
2023-05-06 20:40:33 +0530

    [7] Login route added

--- routes/jwtAuth.js
+++ routes/jwtAuth.js
@@ -29,5 +29,27 @@ router.post("/register",async (req,res)=> {
     }
 })
 
+router.post("/login",async (req,res)=> {
+    try {
+        const { email, password } = req.body;
+        const user  = await pool.query("SELECT * FROM users WHERE user_email = $1", [email]);
+
+        if(user.rows.length === 0){
+            return res.status(401).send("Email or password is incorrect");
+        }
+
+        const validPassword = await bcrypt.compare(password, user.rows[0].user_password);
+        if(!validPassword){
+            return res.status(401).send("Email or password is incorrect");
+        }
+        const token = jwtGenerator(user.rows[0].user_id);
+        res.json({token});
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Server Error");
+    }
+})
+
 
 module.exports = router;
