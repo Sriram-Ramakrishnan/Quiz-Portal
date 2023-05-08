5ccb91687936149b9e2eb8b8fa84be47ea86d171
Sriram Ramakrishnan <srirampro107@gmail.com>
2023-05-08 20:41:58 +0530

    [15] Question fetch route added

--- server/database.sql
+++ server/database.sql
@@ -9,3 +9,16 @@ CREATE TABLE users(
 );
 
 INSERT INTO users (user_name, user_email, user_password) VALUES ('User1','user1@gmail.com','12345678');
+
+CREATE TABLE questionSet_1(
+    q_id SERIAL PRIMARY KEY,
+    question VARCHAR(255) NOT NULL,
+    subject VARCHAR(30) NOT NULL,
+    option1 VARCHAR(255) NOT NULL,
+    option2 VARCHAR(255) NOT NULL,
+    option3 VARCHAR(255) NOT NULL,
+    option4 VARCHAR(255) NOT NULL,
+    correct_answer INTEGER(1) NOT NULL,
+);
+
+INSERT INTO questionSet_1 (question,subject,option1,option2,option3,option4,correct_answer) VALUES ('Question1','CSE','o1','o2','o3','o4',2);
--- server/routes/Dashboard.js
+++ server/routes/Dashboard.js
@@ -12,4 +12,31 @@ router.get("/", authorization, async (req,res) => {
     }
 })
 
+// GET request to fetch questions from the question_Set table
+router.get('/questions', async (req, res) => {
+    try {
+      const { rows } = await pool.query('SELECT * FROM questionSet_1');
+      res.status(200).json(rows);
+    } catch (error) {
+      console.error('Error fetching questions:', error);
+      res.status(500).send('Error fetching questions from database');
+    }
+  });
+  
+  // POST request to insert answers into the answers table
+  router.post('/answers', async (req, res) => {
+    const { answers } = req.body;
+    try {
+      const query = {
+        text: 'INSERT INTO answers (answer) VALUES ($1)',
+        values: [JSON.stringify(answers)],
+      };
+      await pool.query(query);
+      res.status(200).send('Answers submitted successfully');
+    } catch (error) {
+      console.error('Error inserting answers:', error);
+      res.status(500).send('Error inserting answers into database');
+    }
+  });
+
 module.exports = router;
